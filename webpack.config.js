b8cca2db74d7e66abcccc72fb1f30e8ddf45a48f
taipon55 <Taisei127@outlook.com>
2023-02-07 09:10:09 +0900

    webpack-devserverの導入

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,27 @@
 const path = require('path')
 
 module.exports={
-    entry:'./src/index.ts',
+    entry:'./src/index.tsx',
     module:{
         rules:[
             {
-                test:/\.ts$/,
+                test:/\.tsx?$/,
                 use:'ts-loader',
                 exclude:/node_modules/,
             },
         ],
     },
     resolve:{
-        extensions:['.ts'],
+        extensions:['.js','.ts','.tsx'],
     },
     output:{
         path:path.resolve(__dirname,'dist'),
         filename:'index.js',
         publicPath:'dist/'
+    },
+    devServer:{
+        publicPath:'/dist/',
+        hot:true,
+        open:true
     }
 }
